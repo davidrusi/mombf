a4a65265d66675734b9355df386d778a4fabb40a
proebuck <proebuck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-12 05:37:04 +0000

    Reformatted.
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/mombf/pkg/mombf@165 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zellnerbf.lm.R
+++ R/zellnerbf.lm.R
@@ -2,17 +2,35 @@
 ### zellnerbf.lm.R
 ###
 
-zellnerbf.lm <- function(lm1,coef,g,theta0,logbf=FALSE) {
-  
-if (missing(g)) stop('g must be specified')
-if (missing(theta0)) theta0 <- rep(0,length(coef)) else if (length(theta0)!=length(coef)) stop('theta0 must have the same length as coef')
+zellnerbf.lm <- function(lm1,
+                         coef,
+                         g,
+                         theta0,
+                         logbf=FALSE) {
+  if (missing(g)) {
+    stop("'g' must be specified")
+  }
+  if (missing(theta0)) {
+    theta0 <- rep(0, length(coef))
+  } else if (length(theta0) != length(coef)) {
+    stop("'theta0' must have the same length as 'coef'")
+  }
   
   thetahat <- coef(lm1)
   V <- summary(lm1)$cov.unscaled
   n <- length(lm1$residuals); p <- length(thetahat); p1 <- length(coef)
-  if ((min(coef)<1) | (max(coef)>p)) stop('Non-valid value for coef. Use only values between 1 and the number of coefficients in lm1')
+  if ((min(coef)<1) | (max(coef)>p)) {
+    stop("'coef' values must be between 1 and the number of coefficients in 'lm1'")
+  }
   ssr <- sum(residuals(lm1)^2); sr <- sqrt(ssr/(n-p))
-  bf.zellner <- zbfunknown(thetahat[coef],V[coef,coef],n=n,nuisance.theta=p-p1,g=g,theta0=theta0,ssr=ssr,logbf=logbf)
-  return(bf.zellner)
+  bf.zellner <- zbfunknown(thetahat[coef],
+                           V[coef, coef],
+                           n=n,
+                           nuisance.theta=p-p1,
+                           g=g,
+                           theta0=theta0,
+                           ssr=ssr,
+                           logbf=logbf)
+  bf.zellner
 }
 

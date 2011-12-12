61ddd4d40c5e9fdf72d21a816419d4e7e4c1d72a
proebuck <proebuck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-12 05:40:31 +0000

    Reformatted.
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/mombf/pkg/mombf@167 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zellnerbf.knownsig.R
+++ R/zellnerbf.knownsig.R
@@ -2,23 +2,36 @@
 ### zellnerbf.knownsig.R
 ###
 
-zellnerbf.knownsig <- function(theta1hat,V1,n,g=1,theta0,sigma,logbf=FALSE) {
 #Bayes factor based on Zellner's g-prior for linear models (known sigma^2 case).
-# - theta1hat: vector with estimated value of the coefficients that are to be tested
+# - theta1hat: vector with estimated value of the coefficients to be tested
 # - V1: submatrix of covariance corresponding to elements in theta1hat
 # - n: sample size used to fit the model
 # - g: prior parameter
 # - theta0: hypothesized value for theta1hat (defaults to 0)
 # - sigma: residual standard deviation, assumed to be known
 # - logbf: if log==TRUE, the natural logarithm of the Bayes factor is returned
-if (missing(sigma)) stop('sigma must be specified')
-if (missing(theta0)) theta0 <- rep(0,length(theta1hat))
-p1 <- length(theta1hat)
-l <- theta1hat-theta0; l <- matrix(l,nrow=1) %*% solve(V1) %*% matrix(l,ncol=1) * n*g/((1+n*g)*sigma^2) #noncentr param
-muk <- p1+l
-t1 <- matrix(theta1hat-theta0,nrow=1) %*% solve(V1) %*% matrix(theta1hat-theta0,ncol=1) * n*g/((1+n*g)*sigma^2)
-bf <- .5*t1
-if (!logbf) bf <- exp(bf)
-return(bf)
+zellnerbf.knownsig <- function(theta1hat,
+                               V1,
+                               n,
+                               g=1,
+                               theta0,
+                               sigma,
+                               logbf=FALSE) {
+  if (missing(sigma)) {
+    stop("'sigma' must be specified")
+  }
+  if (missing(theta0)) {
+    theta0 <- rep(0, length(theta1hat))
+  }
+  p1 <- length(theta1hat)
+  l <- theta1hat-theta0
+  l <- matrix(l, nrow=1) %*% solve(V1) %*% matrix(l, ncol=1) * n*g/((1+n*g)*sigma^2) #noncentr param
+  muk <- p1+l
+  t1 <- matrix(theta1hat-theta0, nrow=1) %*% solve(V1) %*% matrix(theta1hat-theta0, ncol=1) * n*g/((1+n*g)*sigma^2)
+  bf <- .5*t1
+  if (!logbf) {
+    bf <- exp(bf)
+  }
+  bf
 }
 

d2ce165cbaa208acb4181889ef91e0171d8f2e0c
proebuck <proebuck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-15 14:26:35 +0000

    Reformatted (partial).
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/mombf/pkg/mombf@179 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/pplProbit.R
+++ R/pplProbit.R
@@ -2,47 +2,98 @@
 ### pplProbit.R
 ###
 
+##-----------------------------------------------------------------------------
 ## Find probit model minimizing Posterior Predictive Loss
-pplPM <- function(tauseq=exp(seq(log(.01),log(2),length=20)), kPen=1, y, x, xadj, niter=10^4, thinning=1, burnin=round(niter/10), priorCoef, priorDelta, priorVar, initSearch='greedy', mc.cores=1) {
-if (missing(priorDelta)) { priorDelta <- new("msPriorSpec",priorType='modelIndicator',priorDistr='uniform',priorPars=double(0)) }
-if (missing(priorVar)) { priorVar=new("msPriorSpec",priorType='nuisancePars',priorDistr='invgamma',priorPars=c(alpha=.01,lambda=.01)) }
-if (priorCoef@priorDistr=='pMOM') {
-  nlpfit <- function(tau) {
-    pr <- priorCoef; pr@priorPars['tau'] <- tau
-    fit1 <- pmomPM(y=y,x=x,xadj=xadj,niter=niter,thinning=thinning,burnin=burnin,priorCoef=pr,priorDelta=priorDelta,initSearch=initSearch,verbose=FALSE)
-    p1 <- pplProbit(fit1,x=x,xadj=xadj,y=y,kPen=kPen);
-    return(list(fit1=fit1,ppl=c(p1$d,p1$p,p1$g,p1$msize)))
+pplPM <- function(tauseq=exp(seq(log(.01), log(2), length=20)),
+                  kPen=1,
+                  y,
+                  x,
+                  xadj,
+                  niter=10^4,
+                  thinning=1,
+                  burnin=round(niter/10),
+                  priorCoef,
+                  priorDelta,
+                  priorVar,
+                  initSearch='greedy',
+                  mc.cores=1) {
+  if (missing(priorDelta)) {
+    priorDelta <- new("msPriorSpec",
+                      priorType='modelIndicator',
+                      priorDistr='uniform',
+                      priorPars=double(0))
   }
-} else if (priorCoef@priorDistr=='peMOM') {
-  nlpfit <- function(tau) {
-    pr <- priorCoef; pr@priorPars['tau'] <- tau
-    fit1 <- emomPM(y=y,x=x,xadj=xadj,niter=niter,thinning=thinning,burnin=burnin,priorCoef=pr,priorDelta=priorDelta,initSearch=initSearch,verbose=FALSE)
-    p1 <- pplProbit(fit1,x=x,xadj=xadj,y=y,kPen=kPen);
-    return(list(fit1=fit1,ppl=c(p1$d,p1$p,p1$g,p1$msize)))
+  if (missing(priorVar)) {
+    priorVar <- new("msPriorSpec",
+                    priorType='nuisancePars',
+                    priorDistr='invgamma',
+                    priorPars=c(alpha=.01, lambda=.01))
   }
-} else {
-  stop('Prior on coefficients not recognized. Currently only pMOM and peMOM are implemented')
-}
-#
-if (mc.cores>1) {
-  if ('multicore' %in% loadedNamespaces()) {
-    nlpseq <- multicore::mclapply(as.list(tauseq), nlpfit, mc.cores=mc.cores, mc.preschedule=FALSE)
-  } else stop('multicore library has not been loaded!')
-} else {
-  nlpseq <- lapply(as.list(tauseq), nlpfit)
-}
-PPL <- data.frame(tauseq,do.call(rbind,lapply(nlpseq,'[[','ppl')))
-colnames(PPL) <- c('tau','PPL','G','P','msize')
-require(mgcv)
-gam1 <- gam(PPL ~ s(tau,k=min(10,length(tauseq))), family = Gamma(link = "log"), data=PPL, sp= -1)
-PPL$sPPL <- exp(predict(gam1))
-ans <- list(nlpseq[[which.min(PPL$sPPL)]]$fit1,PPL,PPL$tau[which.min(PPL$PPL)])
-names(ans) <- c('optfit','PPL','tauopt')
-return(ans)
+  if (priorCoef@priorDistr=='pMOM') {
+    nlpfit <- function(tau) {
+      pr <- priorCoef
+      pr@priorPars['tau'] <- tau
+      fit1 <- pmomPM(y=y,
+                     x=x,
+                     xadj=xadj,
+                     niter=niter,
+                     thinning=thinning,
+                     burnin=burnin,
+                     priorCoef=pr,
+                     priorDelta=priorDelta,
+                     initSearch=initSearch,
+                     verbose=FALSE)
+      p1 <- pplProbit(fit1, x=x, xadj=xadj, y=y, kPen=kPen)
+      list(fit1=fit1,
+           ppl=c(p1$d, p1$p, p1$g, p1$msize))
+    }
+  } else if (priorCoef@priorDistr=='peMOM') {
+    nlpfit <- function(tau) {
+      pr <- priorCoef
+      pr@priorPars['tau'] <- tau
+      fit1 <- emomPM(y=y,
+                     x=x,
+                     xadj=xadj,
+                     niter=niter,
+                     thinning=thinning,
+                     burnin=burnin,
+                     priorCoef=pr,
+                     priorDelta=priorDelta,
+                     initSearch=initSearch,
+                     verbose=FALSE)
+      p1 <- pplProbit(fit1, x=x, xadj=xadj, y=y, kPen=kPen)
+      list(fit1=fit1,
+           ppl=c(p1$d, p1$p, p1$g, p1$msize))
+    }
+  } else {
+    stop('prior on coefficients not recognized. Currently only pMOM and peMOM are implemented')
+  }
+
+  #
+  if (mc.cores>1) {
+    if ('multicore' %in% loadedNamespaces()) {
+      nlpseq <- multicore::mclapply(as.list(tauseq), nlpfit, mc.cores=mc.cores, mc.preschedule=FALSE)
+    } else stop('multicore library has not been loaded!')
+  } else {
+    nlpseq <- lapply(as.list(tauseq), nlpfit)
+  }
+  PPL <- data.frame(tauseq, do.call(rbind, lapply(nlpseq, '[[', 'ppl')))
+  colnames(PPL) <- c('tau', 'PPL', 'G', 'P', 'msize')
+  require(mgcv)
+  gam1 <- gam(PPL ~ s(tau, k=min(10, length(tauseq))),
+              family = Gamma(link = "log"),
+              data=PPL,
+              sp= -1)
+  PPL$sPPL <- exp(predict(gam1))
+  ans <- list(nlpseq[[which.min(PPL$sPPL)]]$fit1,
+              PPL,
+              PPL$tau[which.min(PPL$PPL)])
+  names(ans) <- c('optfit', 'PPL', 'tauopt')
+  ans
 }
 
 
-
+##-----------------------------------------------------------------------------
 ## Evaluate Posterior Predictive Loss under a probit model 
 ## Input:
 ## - fit: probit model fit, e.g. as returned by pmomPM
@@ -55,32 +106,36 @@ return(ans)
 ## - D: P + G
 ## - P: P_k(m) - (Penalty), i.e. sum(hm - h(mu))
 ## - G: G_k(m) - (Fit)
-pplProbit <- function(fit, x, xadj, y, kPen=1){
-  m <- nrow(fit$postModel); n <- nrow(x);
+pplProbit <- function(fit, x, xadj, y, kPen=1) {
+  m <- nrow(fit$postModel)
+  n <- nrow(x)
 # generate predictive distribution -------------------------------------
-  yp <- matrix(0, nrow=m, ncol=n);
-  for(i in 1:m){
-    th1 <- fit$postCoef1[i,]; th2 <- fit$postCoef2[i,];
-    lpred <- as.vector(x %*% matrix(th1,ncol=1) + xadj %*% matrix(th2,ncol=1));
-    p <- pnorm(lpred);
-    yp[i,] <- rbinom(n, 1, p);
+  yp <- matrix(0, nrow=m, ncol=n)
+  for (i in 1:m) {
+    th1 <- fit$postCoef1[i, ]
+    th2 <- fit$postCoef2[i, ]
+    lpred <- as.vector(x %*% matrix(th1, ncol=1) + xadj %*% matrix(th2, ncol=1))
+    p <- pnorm(lpred)
+    yp[i, ] <- rbinom(n, 1, p)
   }
 # Compute ppl ----------------------------------------------------------
-  h <- function(z) (z + 0.5)*log(z + 0.5) + (1.5-z)*log(1.5 - z);
-  msize <- mean(apply(fit$postModel, 1, sum)) + ncol(xadj);
+  h <- function(z) (z + 0.5)*log(z + 0.5) + (1.5-z)*log(1.5 - z)
+  msize <- mean(apply(fit$postModel, 1, sum)) + ncol(xadj)
   if (kPen=='msize') kPen <- msize
 # P_k(m) - (Penalty) ---------------------------------------------------
-  mu    <- apply(yp, 2, mean, na.rm=TRUE);
-  hi    <- h(yp);
-  hm    <- apply(hi, 2, mean, na.rm=TRUE);
-  P     <- sum(hm - h(mu));
+  mu    <- apply(yp, 2, mean, na.rm=TRUE)
+  hi    <- h(yp)
+  hm    <- apply(hi, 2, mean, na.rm=TRUE)
+  P     <- sum(hm - h(mu))
 # G_k(m) - (Fit) -------------------------------------------------------
-  Gm    <- (h(mu) + kPen*h(y))/(kPen+1) - h((mu + kPen*y)/(kPen+1));
-  G     <- (kPen+1)*sum(Gm, na.rm=TRUE); 
+  Gm    <- (h(mu) + kPen*h(y))/(kPen+1) - h((mu + kPen*y)/(kPen+1))
+  G     <- (kPen+1)*sum(Gm, na.rm=TRUE) 
 # D_k(m) ---------------------------------------------------------------
-  D = P + G;
+  D = P + G
 # Return Output -------------------------------------------------------- 
-  return(list(d=D, g=G, p=P, msize=msize));
+  list(d=D,
+       g=G,
+       p=P,
+       msize=msize)
 }           
-# END ppl() ----------------------------------------------------------------------------------- # 
 

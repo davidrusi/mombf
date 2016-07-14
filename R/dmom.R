bd43beb10db70d6322036b8902e55bff191a56c2
drossell <drossell@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-07-14 11:05:29 +0000

    Reorganized and simplified file structure
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/mombf/pkg/mombf@268 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/dmom.R
+++ R/dmom.R
@@ -1,3 +1,10 @@
+##############################################################################################
+##
+## DENSITY, QUANTILES, CDF AND PRIOR ELICITATION FOR NON-LOCAL PRIORS
+##
+##############################################################################################
+
+
 ###
 ### dmom.R
 ###
@@ -79,5 +86,262 @@ if (is.vector(V1)) {
   }
 }
 return(ans)
+}
+
+
+
+###
+### dimom.R
+###
+
+#Wrapper to call dpimom (product iMOM) and dqimom (quadratic imom)
+dimom <- function(x, tau=1, phi=1, V1, logscale=FALSE, penalty='product') {
+  if (penalty=='product') {
+    ans <- dpimom(x, tau=tau, phi=phi, logscale=logscale)
+  } else if (penalty=='quadratic') {
+    ans <- dqimom(x, V1=V1, g=tau, n=1, nu=1, logscale=logscale)
+  } else {
+    stop("Only 'penalty==product' and 'penalty==quadratic' are implemented")
+  }
+  return(ans)
+}
+
+#Product iMOM
+
+setMethod("dpimom", signature(x='vector'), function(x, tau=1, phi=1, logscale=FALSE) {
+  ans <- .5*(log(tau)+log(phi)) - lgamma(.5) - log(x^2) - tau*phi/x^2
+  ans[is.nan(ans)] <- -Inf
+  if (!logscale) ans <- exp(ans)
+  ans
+}
+)
+
+setMethod("dpimom", signature(x='matrix'), function(x, tau=1, phi=1, logscale=FALSE) {
+  x2 <- x^2
+  ans <- ncol(x)*(.5*(log(tau)+log(phi)) - lgamma(.5)) - rowSums(log(x2)) - tau*phi*rowSums(1/x2)
+  ans[is.nan(ans)] <- -Inf
+  if (!logscale) ans <- exp(ans)
+  ans
+}
+)
+
+setMethod("dpimom", signature(x='data.frame'), function(x, tau=1, phi=1, logscale=FALSE) {
+  dpimom(as.matrix(x),tau=tau,phi=phi,logscale=logscale)
+}
+)
+
+
+#Quadratic iMOM
+dqimom <- function(x,V1=1,g=1,n=1,nu=1,theta0,logscale=FALSE) {
+if (is.vector(x)) {
+  if (missing(theta0)) theta0 <- 0
+  if (missing(V1)) V1 <- 1
+  qtheta <- (x-theta0)^2/(n*g*V1)
+  k <- -0.5*log(n*g*V1) - lgamma(nu/2)
+  p1 <- 1
+} else {
+  if (missing(theta0)) theta0 <- rep(0,ncol(x))
+  if (missing(V1)) V1 <- diag(ncol(x))
+  qtheta <- t(matrix(x,nrow=nrow(x))) - theta0
+  qtheta <- qtheta %*% solve(n*g*V1) %*% t(qtheta)
+  k <- -0.5*log(n*g*det(V1)) - lgamma(nu/2) + lgamma(ncol(x)/2) - .5*ncol(x)*log(pi)
+  p1 <- ncol(x)
+}
+ans <- (k - .5*(nu+p1)*log(qtheta) -1/qtheta)
+if (!logscale) { ans <- exp(ans); ans[is.na(ans)] <- 0 }
+return(ans)
+}
+
+
+
+
+###
+### pmom.R
+###
+
+pmom <- function(q,V1=1,tau=1) {
+
+  z <- .5-(pnorm(abs(q)/sqrt(V1*tau)) - abs(q)/sqrt(2*pi*V1*tau) * exp(-.5*q^2/(tau*V1)) - .5)
+  return(z*(q<=0)+(1-z)*(q>0))
+}
+
+
+###
+### qmom.R
+###
+
+qmom <- function(p, V1=1, tau=1) {
+
+  e <- function(q) { return((pmom(q, V1, tau)-pneg)^2) }
+  ans <- double(length(p))
+  for (i in 1:length(p)) {
+    pneg <- ifelse(p[i]<=.5, p[i], 1-p[i])
+    ans[i] <- nlminb(start=-1, objective=e)$par
+    if (p[i]>.5) {
+      ans[i] <- -ans[i]
+    }
+  }
+  ans
+}
+
+
+###
+### pimom.R
+###
+
+pimom <- function(q,V1=1,tau=1,nu=1) {
+
+  z <- (.5*pgamma(tau*V1/q^2,nu/2,1))
+  return(z*(q<=0)+(1-z)*(q>0))
+}
+
+
+###
+### qimom.R
+###
+
+qimom <- function(p, V1=1, tau=1, nu=1) {
+
+  ans <- double(length(p))
+  ans[p<=.5] <- -sqrt(tau*V1/qgamma(2*p[p<=.5], nu/2, 1))
+  ans[p>.5] <- sqrt(tau*V1/qgamma(2*(1-p[p>.5]), nu/2, 1))
+  ans
+}
+
+
+
+###
+### emom.R
+###
+
+
+setMethod("demom",signature(x='vector'),function(x, tau, a.tau, b.tau, phi=1, logscale=FALSE) {
+  V1 <- 1
+  if (!missing(tau)) {
+    pen <- -tau*phi/x^2
+    normct <- sqrt(2)
+    ans <- pen + dnorm(x,mean=0,sd=sqrt(tau*phi*V1),log=TRUE) + normct
+  } else {
+    p <- 1; x2phi <- x^2/phi
+    anew <- .5*(a.tau+p); bnew <- .5*(b.tau+x2phi)
+    num <- sqrt(2)*p + .5*a.tau*log(.5*b.tau)
+    den <- lgamma(.5*a.tau) + .5*p*(log(2*pi)+log(phi)) + anew*log(bnew)
+    bt <- bnew/x2phi
+    ans <- num - den + log(2) + .5*anew*log(bt) + log(besselK(sqrt(4*bt),nu=anew,expon.scaled=TRUE)) - sqrt(4*bt)
+  }
+  if (!logscale) ans <- exp(ans)
+  return(ans)
+}
+)
+
+setMethod("demom",signature(x='data.frame'),function(x, tau, a.tau, b.tau, phi=1, logscale=FALSE) {
+  demom(as.matrix(x),tau=tau,a.tau=a.tau,b.tau=b.tau,phi=phi,logscale=logscale)
+}
+)
+
+setMethod("demom",signature(x='matrix'),function(x, tau, a.tau, b.tau, phi=1, logscale=FALSE) {
+  p <- ncol(x)
+  V1 <- diag(p)
+  if (!missing(tau)) {
+    pen <- -tau*phi*rowSums(1/x^2)
+    normct <- p*sqrt(2)
+    ans <- pen + dmvnorm(x,mean=rep(0,p),sigma=tau*phi*V1,log=TRUE) + normct
+  } else {
+    anew <- .5*(a.tau+p); bnew <- .5*(b.tau+rowSums(x^2)/phi)
+    num <- sqrt(2)*p + .5*a.tau*log(.5*b.tau)
+    den <- lgamma(.5*a.tau) + .5*p*(log(2*pi)+log(phi)) + anew*log(bnew)
+    bt <- bnew*phi*rowSums(1/x^2)
+    ans <- num - den + log(2) + .5*anew*log(bt) + log(besselK(sqrt(4*bt),nu=anew,expon.scaled=TRUE)) - sqrt(4*bt)
+  }
+  if (!logscale) ans <- exp(ans)
+  return(ans)
+}
+)
+
+
+pemom <- function(q, tau, a.tau, b.tau) integrate(demom,-Inf,q,tau=tau,a.tau=a.tau,b.tau=b.tau)$value
+
+
+
+##############################################################################################
+## PRIOR ELICITATION
+##############################################################################################
+
+
+###
+### g2mode.R
+###
+
+g2mode <- function(g,
+                   prior=c('iMom', 'normalMom', 'tMom'),
+                   nu=1,
+                   dim=1) {
+  prior <- match.arg(prior)
+  switch(EXPR=prior,
+         normalMom = {
+           2*g
+         },
+         tMom = {
+           g*2*nu/(nu-2+dim)
+         },
+         iMom = {
+           2*g/(nu+dim)
+         })
+}
+
+
+###
+### mode2g.R
+###
+
+mode2g <- function(prior.mode,
+                   prior=c('iMom', 'normalMom', 'tMom'),
+                   nu=1,
+                   dim=1) {
+  prior <- match.arg(prior)
+  switch(EXPR=prior,
+         normalMom = {
+           prior.mode / 2
+         },
+         iMom = {
+           prior.mode * (nu+dim) / 2
+         },
+         tMom = {
+           if (nu<3) {
+             stop('tMom prior must have nu>2 degrees of freedom')
+           }
+           prior.mode * (nu-2+dim) / (2*nu)
+         })
+}
+
+
+###
+### priorp2g.R
+###
+
+priorp2g <- function(priorp,
+                     q,
+                     nu=1,
+                     prior=c('iMom', 'normalMom','tMom')) {
+  prior <- match.arg(prior)
+  switch(EXPR=prior,
+         normalMom = {
+           e <- function(logg) {
+             return((1-2*pmom(-abs(q), tau=exp(logg)) - priorp[i])^2)
+           }
+
+           ans <- double(length(priorp))
+           for (i in 1:length(priorp)) {
+             ans[i] <- exp(nlminb(start=0, objective=e)$par)
+           }
+           ans
+         },
+         tMom = {
+           stop("prior=='tMom' is not currently implemented")
+         },
+         iMom = {
+           p <- (1-priorp)/2
+           qgamma(2*p,nu/2,1)*q^2
+         })
 }
 

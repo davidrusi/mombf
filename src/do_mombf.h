913d7d777b72a5e649d8f6321567860a1468da7f
drossell <drossell@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-03-06 08:48:51 +0000

    Registered entry points R_registerRoutines
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/mombf/pkg/mombf@291 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/do_mombf.h
+++ src/do_mombf.h
@@ -6,6 +6,8 @@
 #define DO_MOMBF_H      1
 
 #include <Rdefines.h>
+#include "R_ext/Rdynload.h"
+
 
 /*
  * Function Prototypes
@@ -40,5 +42,29 @@ extern "C" {
   SEXP nlpMarginalSkewNormI(SEXP Ssel, SEXP Snsel, SEXP Sn, SEXP Sp, SEXP Sy, SEXP Ssumy2, SEXP Sx, SEXP SXtX, SEXP SytX, SEXP Stau, SEXP Staualpha, SEXP Sfixatanhalpha, SEXP Sr, SEXP Smethod, SEXP SoptimMethod, SEXP SB, SEXP Slogscale, SEXP Salpha, SEXP Slambda, SEXP SprCoef);
   SEXP nlpMarginalAlaplI(SEXP Ssel, SEXP Snsel, SEXP Sn, SEXP Sp, SEXP Sy, SEXP Ssumy2, SEXP Sx, SEXP SXtX, SEXP SytX, SEXP Stau, SEXP Staualpha, SEXP Sfixatanhalpha, SEXP Sr, SEXP Ssymmetric, SEXP Smethod, SEXP Shesstype, SEXP SoptimMethod, SEXP SB, SEXP Slogscale, SEXP Salpha, SEXP Slambda, SEXP SprCoef);
 }
+
+static R_CallMethodDef callMethods[]  = {
+  {"eprod_I", (DL_FUNC) &eprod_I, 5},
+  {"pmomLM_I", (DL_FUNC) &pmomLM_I, 36},
+  {"modelSelectionEnumCI", (DL_FUNC) &modelSelectionEnumCI, 27},
+  {"modelSelectionGibbsCI", (DL_FUNC) &modelSelectionGibbsCI, 32},
+  {"greedyVarSelCI", (DL_FUNC) &greedyVarSelCI, 27},
+  {"pmomMarginalKI", (DL_FUNC) &pmomMarginalKI, 14},
+  {"pmomMarginalUI", (DL_FUNC) &pmomMarginalUI, 16},
+  {"pimomMarginalKI", (DL_FUNC) &pimomMarginalKI, 13},
+  {"pimomMarginalUI", (DL_FUNC) &pimomMarginalUI, 15},
+  {"pemomMarginalUI", (DL_FUNC) &pemomMarginalUI, 15},
+  {"zellnerMarginalKI", (DL_FUNC) &zellnerMarginalKI, 11},
+  {"zellnerMarginalUI", (DL_FUNC) &zellnerMarginalUI, 13},
+  {"nlpMarginalSkewNormI", (DL_FUNC) &nlpMarginalSkewNormI, 20},
+  {"nlpMarginalAlaplI", (DL_FUNC) &nlpMarginalAlaplI, 22},
+  {NULL, NULL, 0}
+};
+
+void R_init_myLib(DllInfo *info)
+{
+   R_registerRoutines(info, NULL, callMethods, NULL, NULL);
+}
+
 
 #endif /* DO_MOMBF_H */

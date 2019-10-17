8d67532725aa056d0f1f60d38f9509552bcbfab1
Oriol (ZBook) <oriol.abril.pla@gmail.com>
2019-10-17 12:19:22 +0200

    extend tests on modelSelection

--- tests/testthat/test-modelSelection-gibbs.R
+++ tests/testthat/test-modelSelection-gibbs.R
@@ -1,24 +1,24 @@
-context("Test modelSelection with enumerate=TRUE")
+context("Test modelSelection with Gibbs")
 library("mombf")
 
 source(test_path("data-for-tests.R"))
-tolerance <- 1e-5
+tolerance <- 5e-3
 
 patrick::with_parameters_test_that(
   "modelSelection without groups works for", {
     pDelta <- modelbbprior(1,1)
     log <- capture.output(
-      fit1 <- modelSelection(y=y3, x=X3, priorCoef=pCoef, priorDelta=pDelta, enumerate=TRUE, family=family, priorSkew=pCoef),
-      fit2 <- modelSelection(y3~X3[,2]+X3[,3]+X3[,4], priorCoef=pCoef, priorDelta=pDelta, enumerate=TRUE, family=family, priorSkew=pCoef),
-      fit3 <- modelSelection(as.formula("y~X2+X3+X4"), data=data.frame(X3, y=y3), priorCoef=pCoef, priorDelta=pDelta, enumerate=TRUE, family=family, priorSkew=pCoef)
+      fit1 <- modelSelection(y=y3, x=X3, priorCoef=pCoef, priorDelta=pDelta, enumerate=FALSE, family=family, priorSkew=pCoef),
+      fit2 <- modelSelection(y3~X3[,2]+X3[,3]+X3[,4], priorCoef=pCoef, priorDelta=pDelta, enumerate=FALSE, family=family, priorSkew=pCoef),
+      fit3 <- modelSelection(as.formula("y~X2+X3+X4"), data=data.frame(X3, y=y3), priorCoef=pCoef, priorDelta=pDelta, enumerate=FALSE, family=family, priorSkew=pCoef)
     )
     pp1 <- postProb(fit1)
     pp2 <- postProb(fit2)
     pp3 <- postProb(fit3)
-    expect_equal(pp1$modelid, pp2$modelid)
-    expect_equal(pp1$modelid, pp3$modelid)
-    expect_equal(pp1$pp, pp2$pp, tolerance=tolerance)
-    expect_equal(pp1$pp, pp3$pp, tolerance=tolerance)
+    expect_equal(as.character(pp1$modelid[1]), as.character(pp2$modelid[1]))
+    expect_equal(as.character(pp1$modelid[1]), as.character(pp3$modelid[1]))
+    expect_equal(as.numeric(pp1$pp[1]), as.numeric(pp2$pp[1]), tolerance=tolerance)
+    expect_equal(as.numeric(pp1$pp[1]), as.numeric(pp3$pp[1]), tolerance=tolerance)
   },
   patrick::cases(
     mom_auto=list(family="auto", pCoef=momprior(tau=0.348)),
@@ -43,16 +43,15 @@ patrick::with_parameters_test_that(
     groups <- c(1, 1, 2, 2, 3, 4, 4)
     log <- capture.output(
       fit <- modelSelection(
-        y=y6, x=X6, priorCoef=pCoef, priorDelta=pDelta, enumerate=TRUE,
+        y=y6, x=X6, priorCoef=pCoef, priorDelta=pDelta, enumerate=FALSE,
         family=family, priorSkew=pCoef, priorGroup=pCoef, groups=groups
       )
     )
     pprobs <- postProb(fit)
-    expect_equal(length(pprobs$modelid), 16)
     expect_equal(as.character(pprobs$modelid[1]), "3,4,6,7")
   },
   patrick::cases(
-    # mom_auto=list(family="auto", pCoef=momprior(tau=0.348)),
+    mom_auto=list(family="auto", pCoef=momprior(tau=0.348)),
     mom_normal=list(family="normal", pCoef=momprior(tau=0.348)),
     mom_twopiecenormal=list(family="twopiecenormal", pCoef=momprior(tau=0.348)),
     mom_laplace=list(family="laplace", pCoef=momprior(tau=0.348)),
